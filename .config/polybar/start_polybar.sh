e596ba11915966bc878754fed51bdebb6eed76f5
gabriel@erato <g.sanmartin.silva@gmail.com>
2019-04-21 14:04:13 -0400

    change the direction again, the sources should always be in the github repo!

--- .config/polybar/start_polybar.sh
+++ .config/polybar/start_polybar.sh
@@ -1,1 +1,10 @@
-/home/gabriel/.config/polybar/start_polybar.sh
+#!/usr/bin/env sh
+#this is a comment
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar example -r &
