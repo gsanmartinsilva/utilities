84bbab111a7987c4b0a6419357b83749aac2897f
Gabriel <g.sanmartin.silva@gmail.com>
2019-06-11 11:25:02 -0400

    Some changes

--- Home/.bashrc
+++ Home/.bashrc
@@ -7,11 +7,20 @@
 
 
 # Define Aliases
+alias yt-audio='cd /media/hdd/Media && youtube-dl -x --audio-format mp3'
 alias ls='ls --color=auto'
-alias hdd='cd /mnt/hdd'
-alias v='vim'
-alias dotf='cd repos/dotfiles'
+alias g='gedit'
+alias nvidia-on='sudo tee /proc/acpi/bbswitch <<<ON'
+alias nvidia-off='sudo tee /proc/acpi/bbswitch <<<OFF'
+alias nvidia-status='cat /proc/acpi/bbswitch'
+alias syu='sudo pacman -Syu'
 
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # many commands in ongoing session memory
+export HISTFILESIZE=100000              # many lines in .bash_history
+export QT_QPA_PLATFORMTHEME=qgnomeplatform
 
 #PS1='[\u@\h \W]\$ '
 PS1='\[\e[1;31m\][\[\e[1;33m\]\u\[\e[1;32m\]@\[\e[1;36m\]\h \w\[\e[1;31m\]]$ \[\e[m\]'
+
+. /usr/bin/virtualenvwrapper.sh
--- Scripts/power.sh
+++ Scripts/power.sh
@@ -12,23 +12,23 @@
 # - Give it exec priviledge, or chmod +x /path/to/power.sh
 # - Run it
 
-chosen=$(echo -e "[Cancel]\nShutdown\nReboot\nSuspend\n" | rofi -dmenu -i -l 4 -font "roboto 20" -width 25)
+chosen=$(echo -e "[Cancel]\nShutdown\nReboot\nSuspend\n" | rofi -dmenu -i -l 4  -theme onedark.rasi)
 # Info about some states are available here:
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
 
 if [[ $chosen = "Shutdown" ]]; then
-	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -font "roboto 20" -width 25)
+	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -theme onedark.rasi)
 	if [[ $chosen1 = "Yes" ]]; then
 		systemctl poweroff
 	fi
 elif [[ $chosen = "Reboot" ]]; then
-	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -font "roboto 20" -width 25)
+	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -theme onedark.rasi)
 	if [[ $chosen1 = "Yes" ]]; then
 		systemctl reboot
 	fi
 elif [[ $chosen = "Suspend" ]]; then
-	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -font "roboto 20" -width 25)
+	chosen1=$(echo -e "No\nYes\n" | rofi -dmenu -i -l 2 -theme onedark.rasi)
 	if [[ $chosen1 = "Yes" ]]; then
 		systemctl suspend
 	fi
